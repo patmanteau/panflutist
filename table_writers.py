f9912ede2d51c78cd08ae21d51919df16c844901
Patrick Haas <patmanteau@posteo.de>
2020-03-07 02:10:24 +0100

    refactored Latex writer

--- table_writers.py
+++ table_writers.py
@@ -10,8 +10,6 @@ from enum import Enum
 from string import Template # using .format() is hard because of {} in tex
 import panflute as pf
 
-TABLE_WIDTH = 0.9
-
 LATEX_CELL_TMPL = Template(r"""\begin{minipage}[$outer_alignment]{$cell_width\columnwidth}$cell_alignment
 $body\strut
 \end{minipage}""")
@@ -35,58 +33,72 @@ class OuterAlignment(Enum):
     CENTER = r'c'
     BOTTOM = r'b'
 
-TABULAR_ALIGNMENT = {
-    'AlignDefault': r'l',
-    'AlignLeft': r'l',
-    'AlignRight': r'r',
-    'AlignCenter': r'c'
-}
 
-CELL_ALIGNMENT = {
-    'AlignDefault': r'\raggedright',
-    'AlignLeft': r'\raggedright',
-    'AlignRight': r'\raggedleft',
-    'AlignCenter': r'\centered'
-}
 
-def write_cell(cell, width, align='AlignDefault', outer_align=OuterAlignment.TOP):
-    tex = LATEX_CELL_TMPL.safe_substitute({
-        'outer_alignment': outer_align.value,
-        'cell_width': width * TABLE_WIDTH,
-        'cell_alignment': CELL_ALIGNMENT[align],
-        'body': pf.stringify(cell)
-    })
-    return tex
+class LaTeXWriter(object):
 
-def write_row(row, table, outer_align=OuterAlignment.TOP):
-    cells = []
-    for (i, cell) in enumerate(row.content):
-        cell = write_cell(cell, table.width[i], table.alignment[i], outer_align)
-        cells.append(cell)
+    __slots__ = [
+        'table'
+    ]
 
-    tex = ' & '.join(cells) + '\\tabularnewline'
-    return tex
+    TABULAR_ALIGNMENT = {
+        'AlignDefault': r'l',
+        'AlignLeft': r'l',
+        'AlignRight': r'r',
+        'AlignCenter': r'c'
+    }
 
-def write_table(table, doc):
-    col_desc = ''.join([TABULAR_ALIGNMENT[a] for a in table.alignment])
-    body = ''.join([write_row(row, table) for row in table.content])
-    caption = ''.join([pf.stringify(el) for el in table.caption.list])
-    header = write_row(table.header, table, outer_align=OuterAlignment.BOTTOM)
+    CELL_ALIGNMENT = {
+        'AlignDefault': r'\raggedright',
+        'AlignLeft': r'\raggedright',
+        'AlignRight': r'\raggedleft',
+        'AlignCenter': r'\centered'
+    }
 
-    tex = LATEX_TABLE_TMPL.safe_substitute({
-        'col_descriptor': col_desc,
-        'header': header,
-        'body': body,
-        'caption': caption
-    })
-    return tex
+    TABLE_WIDTH = 0.9
+
+    def __init__(self, table):
+        self.table = table
+
+    def write_cell(self, cell, width, align='AlignDefault', outer_align=OuterAlignment.TOP):
+        tex = LATEX_CELL_TMPL.safe_substitute({
+            'outer_alignment': outer_align.value,
+            'cell_width': width * self.TABLE_WIDTH,
+            'cell_alignment': self.CELL_ALIGNMENT[align],
+            'body': pf.stringify(cell)
+        })
+        return tex
+
+    def write_row(self, row, outer_align=OuterAlignment.TOP):
+        cells = []
+        for (i, cell) in enumerate(row.content):
+            cell = self.write_cell(cell, self.table.width[i], self.table.alignment[i], outer_align)
+            cells.append(cell)
+
+        tex = ' & '.join(cells) + '\\tabularnewline'
+        return tex
+
+    def write_table(self):
+        col_desc = ''.join([self.TABULAR_ALIGNMENT[a] for a in self.table.alignment])
+        body = ''.join([self.write_row(row) for row in self.table.content])
+        caption = ''.join([pf.stringify(el) for el in self.table.caption.list])
+        header = self.write_row(self.table.header, outer_align=OuterAlignment.BOTTOM)
+
+        tex = LATEX_TABLE_TMPL.safe_substitute({
+            'col_descriptor': col_desc,
+            'header': header,
+            'body': body,
+            'caption': caption
+        })
+        return tex
 
 
 def action(table, doc):
     if not isinstance(table, pf.Table) or not doc.format == 'latex':
         return None
     
-    return pf.RawBlock(write_table(table, doc), format='latex')
+    writer = LaTeXWriter(table)
+    return pf.RawBlock(writer.write_table(), format='latex')
 
 
 def main(doc=None):
