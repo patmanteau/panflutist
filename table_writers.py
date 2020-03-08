1a437b1ed4cd0b37711c5d4671bc8976430f7a77
Patrick Haas <patmanteau@posteo.de>
2020-03-08 13:34:42 +0100

    extend Latex table writer

--- table_writers.py
+++ table_writers.py
@@ -2,7 +2,28 @@
 
 r"""
 
-Panflute filter to have captions below longtables for LaTeX. 
+Extended longtables for LaTeX. Enables setting short captions, float placement and table width factor.
+
+Usage:
+ 
+- Wrap tables in a Div of class `ext` and set attributes like so:
+    ```markdown
+    ::: {.ext short="This is a short table caption" placement="htbp" width="0.7"}
+    ---------------------------------------------------------------------------
+    Column 1            Column 2                Column 3
+    --------------      -------------------     -------------------
+    Row 1               0,1                     0,2
+
+    Row 2               0,3                     0,3
+
+    Row 3               0,4                     0,4      
+
+    Row 4               0,5                     0,6
+
+    ---------------------------------------------------------------------------
+    Table: This is the table caption. Suspendisse blandit dolor sed tellus venenatis, venenatis fringilla turpis pretium. \label{ref_a_table}
+    :::
+    ```
 
 """
 
@@ -14,8 +35,8 @@ LATEX_CELL_TMPL = Template(r"""\begin{minipage}[$outer_alignment]{$cell_width\co
 $body\strut
 \end{minipage}""")
 
-LATEX_TABLE_TMPL = Template(r"""\begin{longtable}[]{@{}$col_descriptor@{}}
-\caption{$caption}\endlastfoot
+LATEX_TABLE_TMPL = Template(r"""\begin{longtable}[$placement]{@{}$col_descriptor@{}}
+\caption$short_caption{$caption}\endlastfoot
 \toprule
 $header
 \midrule
@@ -38,7 +59,8 @@ class OuterAlignment(Enum):
 class LaTeXWriter(object):
 
     __slots__ = [
-        'table'
+        'table',
+        'table_width'
     ]
 
     TABULAR_ALIGNMENT = {
@@ -55,15 +77,14 @@ class LaTeXWriter(object):
         'AlignCenter': r'\centered'
     }
 
-    TABLE_WIDTH = 0.9
-
-    def __init__(self, table):
+    def __init__(self, table, table_width=0.9):
         self.table = table
+        self.table_width = table_width
 
     def write_cell(self, cell, width, align='AlignDefault', outer_align=OuterAlignment.TOP):
         tex = LATEX_CELL_TMPL.safe_substitute({
             'outer_alignment': outer_align.value,
-            'cell_width': width * self.TABLE_WIDTH,
+            'cell_width': width * self.table_width,
             'cell_alignment': self.CELL_ALIGNMENT[align],
             'body': pf.stringify(cell)
         })
@@ -79,12 +100,25 @@ class LaTeXWriter(object):
         return tex
 
     def write_table(self):
+        # Process extended table attributes first so cell width gets calculated correctly
+        short_caption = ''
+        placement = ''
+        if isinstance(self.table.parent, pf.Div) and 'ext' in self.table.parent.classes:
+            short_caption = self.table.parent.attributes.get('short')
+            short_caption = '[{}]'.format(short_caption) if short_caption else ''
+            placement = self.table.parent.attributes.get('placement', '')
+            width = self.table.parent.attributes.get('width')
+            if width:
+                self.table_width = float(width)
+
         col_desc = ''.join([self.TABULAR_ALIGNMENT[a] for a in self.table.alignment])
         body = ''.join([self.write_row(row) for row in self.table.content])
         caption = ''.join([pf.stringify(el) for el in self.table.caption.list])
         header = self.write_row(self.table.header, outer_align=OuterAlignment.BOTTOM)
-
+        
         tex = LATEX_TABLE_TMPL.safe_substitute({
+            'placement': placement,
+            'short_caption': short_caption,
             'col_descriptor': col_desc,
             'header': header,
             'body': body,
@@ -95,9 +129,9 @@ class LaTeXWriter(object):
 
 def action(table, doc):
     if not isinstance(table, pf.Table) or not doc.format == 'latex':
-        return None
+        return table
     
-    writer = LaTeXWriter(table)
+    writer = LaTeXWriter(table, 0.9)
     return pf.RawBlock(writer.write_table(), format='latex')
 
 
